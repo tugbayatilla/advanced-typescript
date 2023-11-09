c2b546c9be4448e9f9d0c30681c688d9c8f25b43
Tugbay Atilla <tatilla@igus.net>
2023-11-09 13:51:27 +0100

    learn: how to narrow down the return value with given arguments

--- src/narrow-down.ts
+++ src/narrow-down.ts
@@ -23,26 +23,46 @@ type Config = {
   name: boolean;
   lastname: boolean;
 };
-type User = {
+  type User = {
   name?: string;
   lastname?: string;
 };
 
 // Here declaration to be changed 🔥
-declare function getUser(config: Config): User;
+
+// keyof User                       --> 'name' | 'lastname'
+// "as"                             --> type casting
+
+/* 
+    (C[K] extends true ? K : never) 
+    is a conditional type that checks 
+    if the value of C[K] is assignable to true. 
+    
+    If it is, it includes the key K in the mapped type; 
+    otherwise, it includes never, 
+    effectively excluding that key from the mapped type.
+*/
+
+
+type ConfiguredUser<C extends Config> = {
+    [K in keyof User as (C[K] extends true ? K : never)] -?: User[K]
+}
+
+declare function getUser<C extends Config>(config: C): ConfiguredUser<C>;
 
 // test cases
 const user = getUser({ name: true, lastname: false });
 user.name; // this field should be non-optional
-user.lastname; // this field should not be there and we should have compile error 🛑
+//user.lastname; // this field should not be there and we should have compile error 🛑
 
 const user2 = getUser({ name: true, lastname: true });
 user2.name; // this field should be non-optional
 user2.lastname; // this field should be non-optional
 
 const user3 = getUser({ name: false, lastname: true });
-user3.name; // this field should not be there and we should have compile error 🛑
+//user3.name; // this field should not be there and we should have compile error 🛑
 user3.lastname; // this field should be non-optional
 
 const user4 = getUser({ name: false, lastname: false });
 user4; // user4 should be empty object {}
+ 
